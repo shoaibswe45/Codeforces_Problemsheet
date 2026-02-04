14107caec25faa17fe7d5cc1e57ff7d65ac6dba8
Shoaib Malik <217156799+shoaibswe45@users.noreply.github.com>
2026-02-04 22:51:44 +0600

    Add digits summation functionality in F. Digits summation.c
    Implement digits summation for two input numbers.

---
+++ F. Digits summation.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    long long N,M,X; //kono 2 digit er number k 10 dara vag korle vagfol hisebe tar first digit and vagsesh hisebe tar last digit paoa jay
+//kono tin digit er sonkake 10 dara vag korle vagfol hisebe first dui digit and vagsesh hisebe last digit pao jie
+    scanf("%lld %lld",&N,&M);
+    X=N%10+M%10;
+    printf("%d",X);
+return 0;
+}
